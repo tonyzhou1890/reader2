c4611bf8a8a40fd1a2e19aad463808263357ca65
tony <1332988826@qq.com>
2019-09-10 14:37:56 +0800

    nothing

--- static/js/index.js
+++ static/js/index.js
@@ -5,9 +5,18 @@ class Reader {
     this.option = options
     // 解析 query
     this.query = this._query(window.location.search || '')
-  }
+    // 主要数据存放到 data 中
+    this.data = {
+      // 主界面加载
+      loading: false,
 
-  // 解析 query
+    }
+  }
+  
+  /**
+   * 解析 url 的 search 部分
+   * @param {string} query 
+   */
   _query(query) {
     // 解析正则
     let reg = /([^?=&#]+)=([^&#]*)/g
@@ -24,6 +33,14 @@ class Reader {
       res[item[0]] = item[1]
     })
     return res
+  }
+
+  /**
+   * 默认配置项
+   */
+  defaultOption = {
+    el: document.body,
+    
   }
 }
 
